a8438d4606798649dd282e59af666819cb8fc2b6
Bekzod Akbarov <stx.webdev@gmail.com>
2025-02-27 18:43:36 +0500

    add chaining

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import os
 from dotenv import load_dotenv
 from langchain_openai import OpenAI
+from langchain.prompts import PromptTemplate
+from langchain.chains import LLMChain
 
 # Load environment variables from .env file
 load_dotenv()
@@ -8,6 +10,22 @@ load_dotenv()
 # Get API key from environment variable
 llm = OpenAI(openai_api_key=os.getenv('OPENAI_API_KEY'))
 
-response = llm.invoke('Write a short poem in Uzbek')
+code_prompt = PromptTemplate(
+  template="""
+  You are a helpful assistant that can write code in {language}.
+  Write a function that will {task}.
+  """,
+  input_variables=["language", "task"]
+)
+
+code_chain = LLMChain(
+  llm=llm,
+  prompt=code_prompt
+)
+
+response = code_chain.invoke({
+  "language": "Python",
+  "task": "return the sum of two numbers"
+})
 
 print(response)
