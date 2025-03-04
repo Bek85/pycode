04e37a2fd0e81c5a78497512a19757036da2fd6d
Bekzod Akbarov <stx.webdev@gmail.com>
2025-03-04 14:35:44 +0500

    use ChatBufferMemory to store conversations

--- tchat-gpt.py
+++ tchat-gpt.py
@@ -1,20 +1,31 @@
-from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain.prompts import (
+    HumanMessagePromptTemplate,
+    ChatPromptTemplate,
+    MessagesPlaceholder,
+)
 from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 from dotenv import load_dotenv
 import os
+from langchain.memory import ConversationBufferMemory
 
 load_dotenv()
 
 chat = ChatOpenAI(model="gpt-4o-mini", openai_api_key=os.getenv("OPENAI_API_KEY"))
 
+memory = ConversationBufferMemory(memory_key="chat_history", return_messages=True)
+
 
 prompt = ChatPromptTemplate(
-    input_variables=["content"],
-    messages=[HumanMessagePromptTemplate.from_template("{content}")],
+    input_variables=["content", "chat_history"],
+    messages=[
+        MessagesPlaceholder(variable_name="chat_history"),
+        HumanMessagePromptTemplate.from_template("{content}"),
+    ],
 )
 
-chain = LLMChain(llm=chat, prompt=prompt)
+
+chain = LLMChain(llm=chat, prompt=prompt, memory=memory)
 
 
 while True:
