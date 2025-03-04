a81b8d0e6abebf74cbd616d2d7be452c291a582c
Bekzod Akbarov <stx.webdev@gmail.com>
2025-03-04 10:24:57 +0500

    representing Messages with ChatPromptTemplates

--- tchat-gpt.py
+++ tchat-gpt.py
@@ -1,3 +1,12 @@
+from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate
+
+
+prompt = ChatPromptTemplate(
+    input_variables=["content"],
+    messages=[HumanMessagePromptTemplate.from_template("{content}")],
+)
+
+
 while True:
     user_input = input(">> ")
     print(f"You entered: {user_input}")
