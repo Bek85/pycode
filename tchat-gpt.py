3f40a85c925aa0f4b8baeb4331bdbf0cbd3c91d9
Bekzod Akbarov <stx.webdev@gmail.com>
2025-03-04 10:43:47 +0500

    implement a chat chain

--- tchat-gpt.py
+++ tchat-gpt.py
@@ -1,4 +1,12 @@
 from langchain.prompts import HumanMessagePromptTemplate, ChatPromptTemplate
+from langchain.chat_models import ChatOpenAI
+from langchain.chains import LLMChain
+from dotenv import load_dotenv
+import os
+
+load_dotenv()
+
+chat = ChatOpenAI(model="gpt-4o-mini", openai_api_key=os.getenv("OPENAI_API_KEY"))
 
 
 prompt = ChatPromptTemplate(
@@ -6,7 +14,11 @@ prompt = ChatPromptTemplate(
     messages=[HumanMessagePromptTemplate.from_template("{content}")],
 )
 
+chain = LLMChain(llm=chat, prompt=prompt)
+
 
 while True:
     user_input = input(">> ")
-    print(f"You entered: {user_input}")
+    result = chain.invoke({"content": user_input})
+
+    print(result["text"])
