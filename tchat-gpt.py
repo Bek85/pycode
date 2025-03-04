b5238748a0183f0875b5e5cdbd6a2ecf44731724
Bekzod Akbarov <stx.webdev@gmail.com>
2025-03-04 17:10:01 +0500

    use ConversationSummaryMemory object to summarize conversations

--- tchat-gpt.py
+++ tchat-gpt.py
@@ -7,18 +7,24 @@ from langchain.chat_models import ChatOpenAI
 from langchain.chains import LLMChain
 from dotenv import load_dotenv
 import os
-from langchain.memory import ConversationBufferMemory, FileChatMessageHistory
+from langchain.memory import (
+    ConversationBufferMemory,
+    FileChatMessageHistory,
+    ConversationSummaryMemory,
+)
 
 load_dotenv()
 
-chat = ChatOpenAI(model="gpt-4o-mini", openai_api_key=os.getenv("OPENAI_API_KEY"))
+chat = ChatOpenAI(
+    model="gpt-4o-mini", openai_api_key=os.getenv("OPENAI_API_KEY"), verbose=True
+)
 
-memory = ConversationBufferMemory(
+memory = ConversationSummaryMemory(
+    llm=chat,
     chat_memory=FileChatMessageHistory(file_path="chat_history.json"),
     memory_key="chat_history",
     return_messages=True,
 )
-
 
 prompt = ChatPromptTemplate(
     input_variables=["content", "chat_history"],
@@ -29,7 +35,7 @@ prompt = ChatPromptTemplate(
 )
 
 
-chain = LLMChain(llm=chat, prompt=prompt, memory=memory)
+chain = LLMChain(llm=chat, prompt=prompt, memory=memory, verbose=True)
 
 
 while True:
