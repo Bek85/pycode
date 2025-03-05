f38781c5f0da013bfa5c990a97ffffd8900819e5
Bekzod Akbarov <stx.webdev@gmail.com>
2025-03-05 16:18:12 +0500

    chunking text

--- facts.py
+++ facts.py
@@ -1,11 +1,17 @@
 from dotenv import load_dotenv
 import os
 from langchain.document_loaders import TextLoader
+from langchain.text_splitter import CharacterTextSplitter
 
 
 load_dotenv()
 
-loader = TextLoader("facts.txt")
-documents = loader.load()
+text_splitter = CharacterTextSplitter(separator="\n", chunk_size=200, chunk_overlap=0)
 
-print(documents)
+loader = TextLoader("facts.txt")
+documents = loader.load_and_split(text_splitter)
+
+
+for doc in documents:
+    print(doc.page_content)
+    print("\n")
