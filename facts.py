613fa1c53961eb17be53ab5e0781843734c2b497
Bekzod Akbarov <stx.webdev@gmail.com>
2025-03-06 09:21:05 +0500

    test generating embeddings

--- facts.py
+++ facts.py
@@ -2,9 +2,16 @@ from dotenv import load_dotenv
 import os
 from langchain.document_loaders import TextLoader
 from langchain.text_splitter import CharacterTextSplitter
+from langchain.embeddings import OpenAIEmbeddings
 
 
 load_dotenv()
+
+# embeddings = OpenAIEmbeddings()
+
+# emb = embeddings.embed_query("Hello, world!")
+
+# print(emb)
 
 text_splitter = CharacterTextSplitter(separator="\n", chunk_size=200, chunk_overlap=0)
 
