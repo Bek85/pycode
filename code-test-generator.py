9cde35aa13d417a2442f5e1814f12e2a1001dba6
Bekzod Akbarov <stx.webdev@gmail.com>
2025-03-10 11:17:18 +0500

    fix: langchain chain deprecation fix

--- code-test-generator.py
+++ code-test-generator.py
@@ -3,7 +3,7 @@ from dotenv import load_dotenv
 from langchain_openai import OpenAI
 from langchain.prompts import PromptTemplate
 import argparse
-from langchain.chains import LLMChain, SequentialChain
+from langchain_core.runnables import RunnablePassthrough
 
 
 parser = argparse.ArgumentParser()
@@ -34,14 +34,19 @@ test_prompt = PromptTemplate(
     input_variables=["language", "code"],
 )
 
-# Chain the prompt and the model
-code_chain = LLMChain(llm=llm, prompt=code_prompt, output_key="code")
-test_chain = LLMChain(llm=llm, prompt=test_prompt, output_key="test")
-
-chain = SequentialChain(
-    chains=[code_chain, test_chain],
-    input_variables=["task", "language"],
-    output_variables=["test", "code"],
+# Create the chain using the new pipe syntax
+chain = (
+    RunnablePassthrough()
+    .assign(
+        code=lambda x: (code_prompt | llm).invoke(
+            {"language": x["language"], "task": x["task"]}
+        )
+    )
+    .assign(
+        test=lambda x: (test_prompt | llm).invoke(
+            {"language": x["language"], "code": x["code"]}
+        )
+    )
 )
 
 # Invoke the chain
