f5ef317e2f8df84fb75be86b20b140dd2ce214c4
Bekzod Akbarov <stx.webdev@gmail.com>
2025-03-12 09:42:28 +0500

    create custom retriever

--- prompt.py
+++ prompt.py
@@ -3,10 +3,14 @@ from langchain_openai import OpenAIEmbeddings, ChatOpenAI
 from langchain_chroma import Chroma
 from langchain.chains import RetrievalQA
 
+# import langchain
+
+# langchain.debug = True
+
 
 load_dotenv()
 
-chat = ChatOpenAI()
+chat = ChatOpenAI(model="gpt-4o-mini")
 
 embeddings = OpenAIEmbeddings()
 
@@ -14,7 +18,9 @@ db = Chroma(persist_directory="emb", embedding_function=embeddings)
 
 retriever = db.as_retriever()
 
-chain = RetrievalQA.from_chain_type(llm=chat, chain_type="stuff", retriever=retriever)
+chain = RetrievalQA.from_chain_type(
+    llm=chat, chain_type="stuff", retriever=retriever, verbose=True
+)
 
 result = chain.invoke(
     {"query": "What is the interesting fact about the English language?"}
