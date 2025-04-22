ad6db1e0152f0b59aed591b55a28512e72e932c6
Bekzod Akbarov <stx.webdev@gmail.com>
2025-04-22 18:23:55 +0500

    Update: Fix deprecated OpenAIFunctionsAgent with create_openai_functions_agent

--- agents.py
+++ agents.py
@@ -1,11 +1,12 @@
 from dotenv import load_dotenv
 from langchain.chat_models import init_chat_model
+from langchain.agents import create_openai_functions_agent
 from langchain.prompts import (
     HumanMessagePromptTemplate,
     ChatPromptTemplate,
     MessagesPlaceholder,
 )
-from langchain.agents import OpenAIFunctionsAgent, AgentExecutor
+from langchain.agents import AgentExecutor
 from tools.sql import run_query_tool
 
 
@@ -23,8 +24,8 @@ prompt = ChatPromptTemplate(
     input_variables=["input"],
 )
 
-agent = OpenAIFunctionsAgent(llm=llm, tools=tools, prompt=prompt)
+agent = create_openai_functions_agent(llm, tools, prompt)
 
 agent_executor = AgentExecutor(agent=agent, tools=tools, verbose=True)
 
-agent_executor.invoke({"input": "What is the total number of users in the database?"})
+agent_executor.invoke({"input": "List the columns of the orders table?"})
