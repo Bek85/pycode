2db002e03c68f00d1c9b7ec12e969d714b0fd7e2
Bekzod Akbarov <stx.webdev@gmail.com>
2025-05-22 10:10:17 +0500

    processing text in action

---
+++ rag_basics/text_preprocessing_steps.py
@@ -0,0 +1,18 @@
+import nltk
+from sample_documents import documents
+
+
+def process_text(text):
+    processed_text = text.lower()
+    processed_text = nltk.word_tokenize(processed_text)
+    processed_text = [word for word in processed_text if word.isalnum()]
+
+    stop_words = nltk.corpus.stopwords.words("english")
+    processed_text = [word for word in processed_text if word not in stop_words]
+
+    return processed_text
+
+
+processed_text = [" ".join(process_text(doc)) for doc in documents]
+
+print(processed_text)
