55d27f65183d2a954076707bf60183cf32c33910
Bekzod Akbarov <stx.webdev@gmail.com>
2025-05-29 11:38:11 +0500

    generate text with OpenAI API

--- openai/openai_api.py
+++ openai/openai_api.py
@@ -3,9 +3,26 @@ url = "https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Gfp-wisconsin-m
 
 from openai import OpenAI
 import os
+from IPython.display import Markdown, display
 
 # Define the model
-model = "gpt-4oi"
+model = "gpt-4o"
 
 # Define the client
 client = OpenAI(api_key=os.getenv("OPENAI_API_KEY"))
+
+# Define the user and system prompt
+system_prompt = "You are Steve Jobs, and you are going to brainstorm ideas for a marketing campaign for a new product."
+user_prompt = "Write a list of 10 ideas for a marketing campaign for a new product."
+
+
+# Generate text with OpenAI
+response = client.chat.completions.create(
+    model=model,
+    messages=[
+        {"role": "system", "content": system_prompt},
+        {"role": "user", "content": user_prompt},
+    ],
+)
+
+display(Markdown(response.choices[0].message.content))
